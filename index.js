0bfd26ff86ba40a2035573f7d114889d087b2e0b
ayush-thakur <thakurayush511@gmail.com>
2022-01-06 17:21:16 +0530

    adding more array method

--- index.js
+++ index.js
@@ -103,3 +103,55 @@ let x = arr.keys();
 for(z of x){
  console.log(z);
 }
+//array lenght method return the lenght of array (1 to so on...)
+//array.length
+//array.length = number
+const arr = [1,2,3,4,5];
+let x = arr.lenght = 2;
+console.log(x);
+//array lastIndexOf return the last index (last to start or right to left) for specific value
+//array.lastIndexOf(item, start)
+const arr = [1,2,3,4,5];
+let x = arr.lastIndexOf(2);
+console.log(x);
+//array map method return the result of the function
+//array.map(function(currentValue, index, arr), thisValue)
+
+//in this case return array
+const arr = [{firstname:"riki", lastname:"dhiman"},
+             {firstname:"ramu", lastname:"kumar"}];
+arr.map(function(item){
+console.log([item.firstname ,item.lastname]);
+});
+//in this case return string
+const arr = [{firstname:"riki", lastname:"dhiman"},
+             {firstname:"ramu", lastname:"kumar"}];
+arr.map(function(item){
+console.log([item.firstname ,item.lastname].join("  "));
+});
+//array pop method return the deleted element from last
+//array.pop()
+const arr = [1,2,3,4,5,6];
+let x = arr.pop();
+console.log(x);
+//array prototype method return the new prototype (only for own defined array)
+//Array.prototype.name = value
+function Person(first, last, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.eyeColor = eye;
+  }
+  const myFather = new Person("John", "Doe", "blue");
+  const myMother = new Person("Sally", "Rally", "green");
+  let x = Person.prototype.value = "english";
+  console.log(x);
+  //use case with array
+  const arr = [1,2,3,4,5];
+let x = Array.prototype.arr = "english";
+console.log(x);
+//array push method return the new item in array and also lenght
+//array.push(item1, item2, ..., itemX)
+const arr = [1,2,3,4,5];
+let x = arr.push(6);
+console.log(arr);
+
